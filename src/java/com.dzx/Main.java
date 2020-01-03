ec56ac849689e711e6bdde25f1627958f5822803
戴郑雄 <dzx518@qq.com>
2020-01-03 14:52:52 +0800

    三数之和

--- src/java/com.dzx/Main.java
+++ src/java/com.dzx/Main.java
@@ -1,7 +1,14 @@
 package com.dzx;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Medium3Sum medium3Sum = new Medium3Sum();
+        long start = System.currentTimeMillis();
+        int[] nums = {-2,0,0,2,2};
+        List<List<Integer>> result = medium3Sum.threeSum(nums);
+        System.out.println("time: " + (System.currentTimeMillis() - start) + "ms");
+        System.out.println(result);
     }
 }
---
+++ src/java/com.dzx/Medium3Sum.java
@@ -0,0 +1,48 @@
+package com.dzx;
+
+import java.util.*;
+
+public class Medium3Sum { // 三数之和,双指针
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        int lp, rp, last = nums[0]-1;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (nums[i]==last) {
+                continue;
+            }
+            lp = i+1;
+            rp = nums.length - 1;
+            while (lp < rp) {
+                if (nums[lp] + nums[rp] == -nums[i]) {
+                    result.add(Arrays.asList(nums[i], nums[lp], nums[rp]));
+                    int left = nums[lp];
+                    int right = nums[rp];
+                    lp++;
+                    rp--;
+                    if (lp >= rp) {
+                        break;
+                    }
+                    while (lp < rp && nums[lp] == left) {
+                        lp++;
+                    }
+                    while (rp > lp && nums[rp] == right) {
+                        rp--;
+                    }
+                }else if (nums[lp] + nums[rp] > -nums[i]) {
+                    rp--;
+                }else {
+                    lp++;
+                }
+            }
+            last = nums[i];
+        }
+        return result;
+    }
+}
