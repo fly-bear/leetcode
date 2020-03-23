b3c72fc4682a15d7787faf7c5781f5f19592d69a
戴郑雄 <dzx518@qq.com>
2020-03-23 14:47:06 +0800

    给定链表的中间节点

---
+++ src/java/com.dzx/SimpleMiddleOfTheLinkedList.java
@@ -0,0 +1,37 @@
+package com.dzx;
+
+
+/**
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ * 双指针，一个逐步前进，一个只前进到一半
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class SimpleMiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode mid = head;
+        ListNode point = head;
+        int n = 1;
+        int step = 0;
+        while (point.next!=null) {
+            step++;
+            while (n < step / 2 + 1) {
+                mid = mid.next;
+                n++;
+            }
+            point = point.next;
+        }
+        return mid;
+    }
+}
